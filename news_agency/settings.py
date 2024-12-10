83f6f7172b873f49d241e2194913bbb1c80718c8
Yulia Pylypets <yulia250603@gmail.com>
2024-12-10 11:53:11 +0200

    Update settings and requirements.txt

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0oe*je@r_@umsur@5(_8ni#vcz&plyua7nvl$_f5vi_ttxxjpo"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-0oe*je@r_@umsur@5(_8ni#vcz&plyua7nvl$_f5vi_ttxxjpo")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition

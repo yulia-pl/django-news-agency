d4c0851aa6ef8040655caafcc06073c4a932e26d
Yulia Pylypets <yulia250603@gmail.com>
2024-12-10 14:23:01 +0200

    Add gitkeep

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -13,6 +13,8 @@ import os
 
 from pathlib import Path
 
+from django.contrib import staticfiles
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

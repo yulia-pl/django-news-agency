4a77b16ad55e21c546a97b77d65e7d1138e21325
Yulia Pylypets <yulia250603@gmail.com>
2024-12-10 07:46:29 +0200

    Update URLs to use plural naming conventions

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,18 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('login/', views.AccountLoginView.as_view(), name='login'),
-
-
     path('logout/', views.AccountLogoutView.as_view(), name='logout'),
-
-
     path('register/', views.AccountRegisterView.as_view(), name='register'),
-
-
-    path('profile/', views.AccountDetailView.as_view(), name='profile'),
-
-
-    path('profile/edit/', views.AccountUpdateView.as_view(), name='edit_profile'),
+    path('profiles/', views.AccountDetailView.as_view(), name='profile'),  # Changed to plural
+    path('profiles/edit/', views.AccountUpdateView.as_view(), name='edit_profile'),  # Changed to plural
 ]
